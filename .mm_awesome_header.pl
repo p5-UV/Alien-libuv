7b67540467b2093abbff910f252aab849626c3ac
Chase Whitener <cwhitener@gmail.com>
2018-11-21 11:52:56 -0500

    Update the build a bit
    Update to use version 3 of DZil::PluginBundle::Starter
    Automate version numbers a bit.
    Re-work the try/catch in Makefile.PL
    Update the cpanfile with OS-Specific prereqs.
    Update to libuv v1.24.1
    Add in the -msse2 flags when on Windows/MinGW
    Add more tests to travis
    Fix pod spelling skipwords
    Skip pod test on auto-generated pm

--- .mm_awesome_header.pl
+++ .mm_awesome_header.pl
@@ -1,10 +1,15 @@
 if ($^O eq 'MSWin32') {
     my $ver;
-    my $err = do {
+    my $err;
+    { # catch block
         local $@;
-        eval { require Win32; $ver = (Win32::GetOSVersion())[1]; 1; };
-        $@;
-    };
+        # try
+        $err = $@ || 'Error' unless eval {
+            require Win32;
+            $ver = (Win32::GetOSVersion())[1];
+            1;
+        };
+    }
     if ($err || $ver < 6) {
         die("OS unsupported\nWindows needs to be Win7 or higher.\n");
     }
